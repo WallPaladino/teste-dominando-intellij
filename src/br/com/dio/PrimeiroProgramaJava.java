456a3820b9d57af55a756079b84e5415b513d3ca
WallPaladino <wallevan@gmail.com>
2023-02-22 20:46:14 -0300

    primeiro commit

---
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+/*      int a = 3;
+        int b = 5;
+        System.out.println("A Soma é "+ (a+b));*/
+    }
+}
