761e7081c13d5fef63762c959c15490a20e8c9ed
WallPaladino <wallevan@gmail.com>
2023-02-22 21:07:58 -0300

    criando livro

--- src/br/com/dio/PrimeiroProgramaJava.java
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -6,8 +6,46 @@ public class PrimeiroProgramaJava {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro = new Livro("Senhor dos aneis", 24);
+        System.out.println(livro);
 /*      int a = 3;
         int b = 5;
         System.out.println("A Soma é "+ (a+b));*/
+    }
+}
+
+class Livro{
+
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
